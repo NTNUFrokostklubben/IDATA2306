dd3e4b1c30fa63c047527a3dcb0dedba4bae60c2
matrHS <matthew.r.hunt@gmail.com>
2025-01-20 09:54:59 +0100

    inital commit

---
+++ src/main/java/no/ntnu/learniverseconnect/LearniverseConnectApplication.java
@@ -0,0 +1,13 @@
+package no.ntnu.learniverseconnect;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearniverseConnectApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(LearniverseConnectApplication.class, args);
+  }
+
+}
---
+++ src/test/java/no/ntnu/learniverseconnect/LearniverseConnectApplicationTests.java
@@ -0,0 +1,13 @@
+package no.ntnu.learniverseconnect;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearniverseConnectApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
