07c95ca28fcafc25008dc712cf1acadb38241954
augoks <augustdo@stud.ntnu.no>
2025-02-25 14:22:58 +0100

    get and post to courseController and CourseProviderController

--- src/main/java/no/ntnu/learniverseconnect/controllers/CourseController.java
+++ src/main/java/no/ntnu/learniverseconnect/controllers/CourseController.java
@@ -4,7 +4,9 @@ import java.util.List;
 import no.ntnu.learniverseconnect.model.entities.Course;
 import no.ntnu.learniverseconnect.model.repos.CourseRepo;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -17,18 +19,45 @@ public class CourseController {
 
   CourseRepo repo;
 
+  /**
+   * Creates a new instance of the CourseController using JPA black magic.
+   *
+   * @param repo the course repo interface.
+   */
   @Autowired
   public CourseController(CourseRepo repo) {
     this.repo = repo;
   }
 
+  /**
+   * Returns a list of all courses.
+   *
+   * @return a list of all courses.
+   */
   @GetMapping("/courses")
-  public List<Course> getCourses() {
-    return repo.findAll();
+  public ResponseEntity<List<Course>> getCourses() {
+    return ResponseEntity.status(200).body(repo.findAll());
   }
 
+  /**
+   * Adds a course to the database.
+   *
+   * @param course the course to add.
+   */
   @PostMapping("/course")
-    public void addCourse(@RequestBody Course course) {
-      repo.save(course);
-    }
+  public void addCourse(@RequestBody Course course) {
+    repo.save(course);
+  }
+
+  /**
+   * Returns a course with the given id.
+   *
+   * @param id the id of the course to return.
+   * @return the course with the given id.
+   */
+
+  @GetMapping("/course/{id}")
+  public ResponseEntity<Course> getCourse(@PathVariable int id) {
+    return ResponseEntity.status(200).body(repo.findById(id).orElse(null));
+  }
 }
--- src/main/java/no/ntnu/learniverseconnect/controllers/CourseProviderController.java
+++ src/main/java/no/ntnu/learniverseconnect/controllers/CourseProviderController.java
@@ -1,4 +1,50 @@
 package no.ntnu.learniverseconnect.controllers;
 
+import java.util.List;
+import no.ntnu.learniverseconnect.model.entities.CourseProvider;
+import no.ntnu.learniverseconnect.model.repos.CourseProviderRepo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class CourseProviderController {
+
+  CourseProviderRepo repo;
+
+  @Autowired
+  public CourseProviderController(CourseProviderRepo repo) {
+    this.repo = repo;
+  }
+
+  /**
+   * Returns a list of all course providers.
+   * @return a list of all course providers.
+   */
+  @GetMapping("/providers")
+    public ResponseEntity<List<CourseProvider>> getProviders() {
+        return ResponseEntity.status(200).body(repo.findAll());
+    };
+  /**
+   * Returns a course provider with the given id.
+   * @param id the id of the course provider to return.
+   * @return the course provider with the given id.
+   */
+  @GetMapping("/provider/{id}")
+    public ResponseEntity<CourseProvider> getProvider(@PathVariable int id) {
+        return ResponseEntity.status(200).body(repo.findById(id).orElse(null));
+    };
+
+    /**
+     * Adds a course provider to the database.
+     * @param provider the course provider to add.
+     */
+  @PostMapping("/provider")
+    public void addProvider(@RequestBody CourseProvider provider) {
+        repo.save(provider);
+    };
 }
