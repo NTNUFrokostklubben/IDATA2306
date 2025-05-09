b72ca3710f982df23375b2422adde91bfde8f59c
augoks <augustdo@stud.ntnu.no>
2025-05-09 14:35:34 +0200

    changed the user class to be initialized with standard image

--- src/main/java/no/ntnu/learniverseconnect/model/entities/User.java
+++ src/main/java/no/ntnu/learniverseconnect/model/entities/User.java
@@ -61,6 +61,7 @@ public class User {
   @PrePersist
   protected void onCreate() {
     this.userCreated = new Timestamp(System.currentTimeMillis());
+    this.profilePicture ="http://localhost:8081/uploads/images/default_img.png";
   }
 
   /**
